8ba710aa8e687dfefcab325b18acb0a163e5761d
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-03-25 17:14:20 +0000

    Added splish splash test.

--- simulators/splishsplash/v2.13.0/test_sim.sh
+++ simulators/splishsplash/v2.13.0/test_sim.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cd /home/schism-input-example
-mkdir outputs
+mkdir /home/splishsplash-input-example/output
 
-/schism/build/bin/pschism && \
-
-# Check if the simulation ran successfully
-# Schism can return 0 if an error occurs, so we need to check the output
-grep "********CG Solve at timestep     2880" outputs/JCG.out
+/SPlisHSPlasH_CPU/bin/SPHSimulator /home/splishsplash-input-example/config.json --output-dir /home/splishsplash-input-example/output --no-gui
