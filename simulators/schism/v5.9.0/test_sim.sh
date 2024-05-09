7bde6f8ed7cfa863b622a7a24548866f6372cf43
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-05-09 13:08:31 +0100

    Update test_sim.sh

--- simulators/schism/v5.9.0/test_sim.sh
+++ simulators/schism/v5.9.0/test_sim.sh
@@ -4,7 +4,6 @@ cd /home/schism-input-example
 mkdir outputs
 
 /schism/build/bin/pschism && \
-
 # Check if the simulation ran successfully
 # Schism can return 0 if an error occurs, so we need to check the output
 grep "********CG Solve at timestep     2880" outputs/JCG.out
