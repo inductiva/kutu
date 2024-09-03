ecccd812741badf98a728eddf7f448478854c659
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-09-03 12:31:01 +0100

    Changed fvcom for the default case and version 5.1

--- simulators/fvcom/v4.4.7/test_sim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/fds-input-example
-
-fds mccaffrey.fds
-
-# Since fds returns 0 even if the simulation fails, we need to check the output 
-# file to know if the simulation was successful.
-grep -q "FDS completed successfully" mccaffrey.out 
---
+++ simulators/fvcom/v5.1.0/test_sim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/fvcom-input-example/run
+
+cp /FVCOM/src/fvcom .
+
+./fvcom --CASETEST=tst
