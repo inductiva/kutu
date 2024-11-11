6cf99a18355c0ceab5c8d4fcf829a65e7b2648fd
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-11-11 16:09:05 +0000

    update test_sim

--- simulators/openfoam-esi/v2206/test_sim.sh
+++ simulators/openfoam-esi/v2206/test_sim.sh
@@ -2,10 +2,10 @@
 
 cd /home/openfoam-esi-input-example
 
-/launch.sh "surfaceFeatureExtract" && \
-/launch.sh "blockMesh" && \
-/launch.sh "decomposePar -copyZero" && \
+surfaceFeatureExtract && \
+blockMesh && \
+decomposePar -copyZero && \
 cp -r 0.orig/ 0 && \
-/launch.sh "snappyHexMesh -overwrite" && \
-/launch.sh "potentialFoam" && \
-/launch.sh "simpleFoam"
+snappyHexMesh -overwrite && \
+potentialFoam && \
+simpleFoam
