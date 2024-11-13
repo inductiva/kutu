13623df900ad05227d09104ec1903da898ecbf28
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-11-13 21:06:32 +0000

    Moving runFunctions to bin one by one

--- simulators/openfoam-esi/v2206/launch.sh
+++ simulators/openfoam-esi/v2206/launch.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-source /OpenFOAM-v2206/etc/bashrc
-source /OpenFOAM-v2206/bin/tools/RunFunctions
-
-# RUN the command
-exec "$@"
+$1
--- simulators/openfoam-esi/v2406/launch.sh
+++ simulators/openfoam-esi/v2406/launch.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-source /OpenFOAM-v2406/etc/bashrc
-source /OpenFOAM-v2406/bin/tools/RunFunctions
-
-# RUN the command
-exec "$@"
+$1
