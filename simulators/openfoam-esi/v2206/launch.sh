b7927a8c2799beb116ab338ca17303afcc2f7f86
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-11-13 17:37:25 +0000

    moved sources into entrypoint file

--- simulators/openfoam-esi/v2206/launch.sh
+++ simulators/openfoam-esi/v2206/launch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+source /OpenFOAM-v2206/etc/bashrc
+source /OpenFOAM-v2206/bin/tools/RunFunctions
+
 # RUN the command
-$1
+exec "$@"
--- simulators/openfoam-esi/v2406/launch.sh
+++ simulators/openfoam-esi/v2406/launch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+source /OpenFOAM-v2406/etc/bashrc
+source /OpenFOAM-v2406/bin/tools/RunFunctions
+
 # RUN the command
-$1
+exec "$@"
