2590b8ad5229e822bb94e4b4f34cbbdfd212379f
Ayomide Adekoya <ayomideadekoya@Ayomides-MacBook-Air.local>
2025-01-08 17:04:59 +0000

    adding seissol simulator

--- simulators/seissol/v1.3.0/test_sim.sh
+++ simulators/seissol/v1.3.0/test_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-# jupyter lab --ip=0.0.0.0 --port=53155 --no-browser --allow-root
-mpirun --allow-run-as-root --oversubscribe --use-hwthread-cpus -np 2 SeisSol_proxy_Release_dhsw_4_elastic /home/seissol/examples/tpv33/parameters.par
-# mpiexec -np 2 /home/tools/bin/SeisSol_Release_dhsw_4_elastic /home/seissol/examples/tpv33/parameters.par
+jupyter lab --ip=0.0.0.0 --port=53155 --no-browser --allow-root
+# mpirun --allow-run-as-root --oversubscribe --use-hwthread-cpus -np 2 SeisSol_Release_dhsw_4_elastic /home/seissol/examples/tpv33/parameters.par
+#mpiexec -np 2 /home/tools/bin/SeisSol_Release_dhsw_4_elastic /home/seissol/examples/tpv33/parameters.par
