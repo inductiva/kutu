5cb40445c2db580919067cd14eb993fe86bb7fb0
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-02-24 16:36:26 +0000

    Add opensees py

---
+++ simulators/openseespy/v3.7.1/test_sim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python Example1.1.py
+python Example2.1.py
+python Example3.1.py
+python Example3.2.py
+python Example3.3.py
+python Example4.1.py
+python Example5.1.py
+python Example6.1.py
+python Example7.1.py
+python Example8.1.py
+mpirun -np 2 python example_mpi_paralleltruss_explicit.py
