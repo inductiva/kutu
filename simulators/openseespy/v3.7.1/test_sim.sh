5cc2362b0ef7c21dec1946428d7af520a121fd5c
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-02-24 16:42:59 +0000

    Update test_sim.sh

--- simulators/openseespy/v3.7.1/test_sim.sh
+++ simulators/openseespy/v3.7.1/test_sim.sh
@@ -10,4 +10,4 @@ python Example5.1.py
 python Example6.1.py
 python Example7.1.py
 python Example8.1.py
-mpirun -np 2 python example_mpi_paralleltruss_explicit.py
+mpirun -np 2 --allow-run-as-root python example_mpi_paralleltruss_explicit.py
