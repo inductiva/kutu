f8812cb71118d8628782c99d7dbb5a4a2a32d6e4
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-04-14 17:15:15 +0100

    Fix test

--- simulators/dualsphysics/v5.4.1/test_sim.sh
+++ simulators/dualsphysics/v5.4.1/test_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/dualsphysics-input-example
+cd /home/dualsphysicsv5.4.1-input-example
 
 /DualSPHysics/bin/linux/gencase config flow_cylinder -save:all && \
 /DualSPHysics/bin/linux/dualsphysics5.4cpu flow_cylinder flow_cylinder -dirdataout data -svres && \
