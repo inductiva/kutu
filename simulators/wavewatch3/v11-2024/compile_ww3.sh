ada726c98f903cf8aa2ac90b1abf019cc0edd181
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-08-04 10:46:43 +0100

    Improve compilation script to allow custom switches

--- simulators/wavewatch3/v11-2024/compile_ww3.sh
+++ simulators/wavewatch3/v11-2024/compile_ww3.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Usage: bash script.sh Ifremer2
+# Usage: bash script.sh Ifremer2 [true|false]
+
+# if the second argument is true we consider the switch to be a custom switch
+# provided by the user, and will copy it to the model/bin dir
+# otherwise we assume it is a switch provided by ww3 and should already be there
 
 # Exit on any error
 set -e
@@ -25,6 +29,15 @@ fi
 
 SWITCH_NAME=$1
 echo "[DEBUG] SWITCH_NAME set to: $SWITCH_NAME"
+IS_CUSTOM="$2"
+
+# If IS_CUSTOM is set to "true" (case-insensitive), copy the switch file
+if [[ "${IS_CUSTOM,,}" == "true" ]]; then
+  echo "[DEBUG] IS_CUSTOM is true, copying custom switch..."
+  BASENAME=$(basename "$SWITCH_NAME")
+  cp -f "/workdir/output/artifacts/$SWITCH_NAME" "model/bin/switch_${BASENAME}"
+  echo "[DEBUG] Copied $SWITCH_NAME to model/bin/switch_${BASENAME}"
+fi
 
 echo "[DEBUG] Removing any existing build directory..."
 rm -rf build
