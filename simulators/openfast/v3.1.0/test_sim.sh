a7a77bb165e27ffed9c8b34714a7a51710829df4
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-09-24 10:57:23 +0100

    update test

--- simulators/openfast/v3.1.0/test_sim.sh
+++ simulators/openfast/v3.1.0/test_sim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/openfast-input-example
+cd /home/openfastv3.1.0-input-example
 
-openfast ./IEA-15-240-RWT-Monopile.fst
+openfast ./5MW_OC4Semi_WSt_WavesWN/5MW_OC4Semi_WSt_WavesWN.fst
