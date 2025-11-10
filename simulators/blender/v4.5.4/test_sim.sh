00475e86705188f534f0e14a481f414802f7975a
rcvalerio <rvalerio@inductiva.ai>
2025-11-10 17:43:03 +0000

    Revert "Add blender"
    This reverts commit 05bc5b7a0d239cc39d5bf675d6b14267b2663fc5.

--- simulators/blender/v4.5.4/test_sim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Test Blender installation by checking version
-blender --version
-
-# Run Blender in headless mode with a simple Python script to verify it works
-blender --background --python-expr "import bpy; print('Blender is working correctly')"
-
