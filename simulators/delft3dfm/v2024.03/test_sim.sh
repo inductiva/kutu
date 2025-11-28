7e41c1a52ca487cce8d37d2417173067d051a19c
rcvalerio <rvalerio@inductiva.ai>
2025-11-28 09:24:12 +0000

    Load test example from inductiva storage

--- simulators/delft3dfm/v2024.03/test_sim.sh
+++ simulators/delft3dfm/v2024.03/test_sim.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Download D-Flow FM example from Deltares SVN
-apt-get update && apt-get install -y subversion
-svn export --trust-server-cert-failures=unknown-ca --non-interactive \
-    https://svn.oss.deltares.nl/repos/delft3d/tags/delft3dfm/142431/examples/12_dflowfm/test_data/e100_f02_c02-FriesianInlet_schematic_FM \
-    /tmp/dflowfm-example
+# Install curl, unzip and ca-certificates
+apt-get update && apt-get install -y --no-install-recommends curl unzip ca-certificates
+
+# Download and unzip D-Flow FM example
+curl -L -o /tmp/dflowfm-example.zip https://storage.googleapis.com/inductiva-api-demo-files/delft3dfm-input-example.zip
+unzip /tmp/dflowfm-example.zip -d /tmp
 
 cd /tmp/dflowfm-example
 
