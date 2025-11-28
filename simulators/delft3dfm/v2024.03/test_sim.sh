33938a3e5f26c4657065f64410d798f2ccfdd27b
rcvalerio <rvalerio@inductiva.ai>
2025-11-28 10:25:49 +0000

    Update test zip format

--- simulators/delft3dfm/v2024.03/test_sim.sh
+++ simulators/delft3dfm/v2024.03/test_sim.sh
@@ -4,10 +4,10 @@
 apt-get update && apt-get install -y --no-install-recommends curl unzip ca-certificates
 
 # Download and unzip D-Flow FM example
-curl -L -o /tmp/dflowfm-example.zip https://storage.googleapis.com/inductiva-api-demo-files/delft3dfm-input-example.zip
-unzip /tmp/dflowfm-example.zip -d /tmp
+curl -L -o /tmp/delft3dfm-input-example.zip https://storage.googleapis.com/inductiva-api-demo-files/delft3dfm-input-example.zip
+unzip /tmp/delft3dfm-input-example.zip -d /tmp
 
-cd /tmp/dflowfm-example
+cd /tmp/delft3dfm-input-example
 
 # Run D-Flow FM simulation
 dflowfm --autostartstop f34.mdu
